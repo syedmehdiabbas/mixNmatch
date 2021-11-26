ba9fbae53472d8b2f138368e74dce26ef9862391
Mehdi <mehdi110abbas@gmail.com>
2021-11-26 22:25:42 +0530

    build fix

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyCiCDPFLhjSrNqE9pRdhu7buXfk5RK93YA',
+  authDomain: 'mixnmatch2-60673.firebaseapp.com',
+  projectId: 'mixnmatch2-60673',
+  storageBucket: 'mixnmatch2-60673.appspot.com',
+  messagingSenderId: '501959382630',
+  appId: '1:501959382630:web:15f7e17ebdeccd49a4ffa7',
+};
